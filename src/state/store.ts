7935b4aa05113b1ae9b1ce03ef0214f088cb9d92
Alexandr <puls.ek@rambler.ru>
2023-03-23 10:24:51 +0300

    up 23033023 6

--- src/state/store.ts
+++ src/state/store.ts
@@ -15,7 +15,7 @@ export const rootReducer = combineReducers({
     auth: authReducer
 });
 
-/////// Login 01010 1 /////////
+/////// Login 01010 12345 /////////
 
 ///////////////////////////////////////////////////////////////////////
 //export type RootActions = TodoActionsTypes | TaskActionsTypes;
